4cd04982a9c2c02f83235597ffd9f4c93d91ab5c
commanje <jeremy.commande@hotmail.fr>
2017-02-27 11:40:13 +0100

    Avec envoi message

---
+++ app/src/main/java/com/example/commanje/channelmessaging2/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.commanje.channelmessaging2;
+
+/**
+ * Created by commanje on 06/02/2017.
+ */
+public class AppConfig {
+
+    public static final String PREFS_NAME = "Stockage";
+}
---
+++ app/src/main/java/com/example/commanje/channelmessaging2/RowMessage.java
@@ -0,0 +1,14 @@
+package com.example.commanje.channelmessaging2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RowMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_row_message);
+    }
+
+}
